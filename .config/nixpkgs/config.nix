f42bda7e35dc431832ca53846455f77a198b3bf5
Amy de Buitléir <amy@nualeargais.ie>
2019-01-02 14:06:30 +0000

    Added more packages to overlay

--- .config/nixpkgs/config.nix
+++ .config/nixpkgs/config.nix
@@ -1,14 +1,17 @@
 {
+  allowUnfree = true;
+
   packageOverrides = super: let self = super.pkgs; in
   {
     haskellPackages = super.haskellPackages.override {
       overrides = self: super: {
+        gray-extended = self.callPackage ~/x/gray-extended {};
         grid = self.callPackage ~/x/grid {};
-        # grid = self.callPackage ~/x/som {};
-        # grid = self.callPackage ~/x/creatur {};
-        # grid = self.callPackage ~/x/creatur-wains {};
-        # grid = self.callPackage ~/x/creatur-wains-test-utils {};
-        # grid = self.callPackage ~/x/creatur-dvector-wains {};
+        som = self.callPackage ~/x/som {};
+        creatur = self.callPackage ~/x/creatur {};
+        creatur-wains = self.callPackage ~/x/creatur-wains {};
+        creatur-wains-test-utils = self.callPackage ~/x/creatur-wains-test-utils {};
+        creatur-dvector-wains = self.callPackage ~/x/creatur-dvector-wains {};
       };
     };
   };
