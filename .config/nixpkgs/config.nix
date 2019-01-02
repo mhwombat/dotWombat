c62a2f7ee9b90d1ecde31713f721dc54c9fb29e8
Amy de Buitléir <amy@nualeargais.ie>
2019-01-02 08:23:25 +0000

    Added wain stuff to overlay

--- .config/nixpkgs/config.nix
+++ .config/nixpkgs/config.nix
@@ -3,7 +3,12 @@
   {
     haskellPackages = super.haskellPackages.override {
       overrides = self: super: {
-        grid = self.callPackage ~/grid {};
+        grid = self.callPackage ~/x/grid {};
+        # grid = self.callPackage ~/x/som {};
+        # grid = self.callPackage ~/x/creatur {};
+        # grid = self.callPackage ~/x/creatur-wains {};
+        # grid = self.callPackage ~/x/creatur-wains-test-utils {};
+        # grid = self.callPackage ~/x/creatur-dvector-wains {};
       };
     };
   };
