383c66bdaddc6da45a117f2adc18e881e5e86b4e
Amy de Buitléir <amy@nualeargais.ie>
2019-04-19 22:48:14 +0100

    improved idris-repl key bindings

--- .emacs.d/idris-repl.el
+++ .emacs.d/idris-repl.el
@@ -3,6 +3,6 @@
 ;;
 
 (defun add-idris-repl-key-bindings-hook ()
-  (global-set-key (kbd "<up>") 'idris-repl-backward-history)
-  (global-set-key (kbd "<down>") 'idris-repl-forward-history))
+  (local-set-key (kbd "<up>") 'idris-repl-backward-history)
+  (local-set-key (kbd "<down>") 'idris-repl-forward-history))
 (add-hook 'idris-repl-mode-hook 'add-idris-repl-key-bindings-hook)
