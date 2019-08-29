b7f313d1f2d696d06799edffb398cb76bcc1283d
Amy de Buitléir <amy@nualeargais.ie>
2019-08-29 17:49:22 +0100

    added gensim

--- nixos/python3.nix
+++ nixos/python3.nix
@@ -2,6 +2,7 @@
   nixpkgs.config.packageOverrides = pkgs: with pkgs; {
     python3Env = python36.withPackages (ps: with ps; [
       csvkit
+      gensim
       h5py
       ipython
       JayDeBeApi
