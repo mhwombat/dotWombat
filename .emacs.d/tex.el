7a8a5bdc2bb6fef0d9f4703e8ba4ebb5aaca9e62
Amy de Buitléir <amy@nualeargais.ie>
2019-09-04 14:49:18 +0100

    fixed mode hook

--- .emacs.d/tex.el
+++ .emacs.d/tex.el
@@ -3,13 +3,17 @@
 ;;
 
 (defun add-tex-key-bindings-hook ()
+  ;; (message "latex-mode-hook invoked")
   (defhydra hydra-tex (:exit t)
     "tex mode"
-    ("s" (insert "Hi Wombat!") "source")
+    ("s" LaTeX-section "section/chapter")
+    ("e" LaTeX-environment "environment")
   )
   (local-set-key (kbd "<menu>") 'hydra-tex/body)
 )
-(add-hook 'latex-mode-hook 'add-tex-key-bindings-hook)
+(add-hook 'LaTeX-mode-hook 'add-tex-key-bindings-hook)
+;; LaTeX-mode-hook is used by AUCTeX's LaTeX mode.
+;; latex-mode-hook is used by Emacs' built-in latex mode.
 
 ;; AUCTeX
 (setq TeX-auto-save t)
