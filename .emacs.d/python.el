eb9ebb48fa260b72e52ce984fd0965816499c8c7
Amy de Buitléir <amy@nualeargais.ie>
2020-04-14 15:20:56 +0100

    add format to hydra

--- .emacs.d/python.el
+++ .emacs.d/python.el
@@ -8,3 +8,15 @@
 ;; Remove trailing whitespace
 (add-hook 'python-mode-hook 'add-remove-trailing-whitespace-hook)
 
+;; Code formatter
+(setq enable-blacken-mode t)
+
+(defun add-python-key-bindings-hook ()
+  ;; (message "python-mode-hook invoked")
+  (defhydra hydra-python (:exit t)
+    "python mode"
+    ("f" blacken-buffer "format")
+    )
+  (local-set-key (kbd "<menu>") 'hydra-python/body)
+)
+(add-hook 'python-mode-hook 'add-python-key-bindings-hook)
