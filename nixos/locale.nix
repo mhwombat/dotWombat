54ff08f5916f0e42ac845ea89fde66bbeb43830c
Amy de Buitléir <amy@nualeargais.ie>
2020-04-21 14:49:58 +0100

    downgraded to NixOS 19.09

--- nixos/locale.nix
+++ nixos/locale.nix
@@ -2,13 +2,10 @@
 
 {
   # Select internationalisation properties.
-  console = {
-    font = "Lat2-Terminus16";
-    keyMap = "uk";
-  };
-
   i18n = {
-      defaultLocale = "en_IE.UTF-8";
+    consoleFont = "Lat2-Terminus16";
+    consoleKeyMap = "uk";
+    defaultLocale = "en_IE.UTF-8";
   };
 
   # Set your time zone.
