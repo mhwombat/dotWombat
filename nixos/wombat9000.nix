bb5523042451bfa83b7d2478dcb2c5a08174bc4a
Amy de Buitléir <amy@nualeargais.ie>
2020-05-16 23:58:08 +0100

    attempt wifi

--- nixos/wombat9000.nix
+++ nixos/wombat9000.nix
@@ -3,4 +3,12 @@
 {
   # Define on which hard drive you want to install Grub.
   boot.loader.grub.device = "/dev/sda"; # or "nodev" for efi only
+
+  networking.wireless.enable = true;
+
+  networking.wireless.networks = {
+    "eircom-40689925 5G" = {
+      psk = "xxxxxxxxx";
+    };
+  };
 }
