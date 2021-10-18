44c8c8ce42982f737d714afcc50498e07b681779
Amy de Buitléir <amy@nualeargais.ie>
2021-10-18 14:10:16 +0100

    set hostname

--- nixos/wombat11k.nix
+++ nixos/wombat11k.nix
@@ -6,6 +6,8 @@
   boot.loader.efi.canTouchEfiVariables = true;
 
   networking = {
+    hostName = "wombat11k";
+
     # The global useDHCP flag is deprecated, therefore explicitly set to false here.
     # Per-interface useDHCP will be mandatory in the future, so this generated config
     # replicates the default behaviour.
