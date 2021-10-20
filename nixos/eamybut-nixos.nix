54ead02a0a2bb2faa8a0e069634c11b2da1cf779
Amy de Buitléir <amy@nualeargais.ie>
2021-10-20 14:15:02 +0100

    no longer used

--- nixos/eamybut-nixos.nix
+++
@@ -1,9 +0,0 @@
-{ config, pkgs, ... }:
-
-{
-  # Define on which hard drive you want to install Grub.
-  boot.loader.grub.device = "/dev/sda"; # or "nodev" for efi only
-
-  # Remove the fsck that runs at startup; it fails on a VirtualBox guest
-  boot.initrd.checkJournalingFS = false;
-}
--- nixos/python2.nix
+++
@@ -1,8 +0,0 @@
-{
-  nixpkgs.config.packageOverrides = pkgs: with pkgs; {
-    python2Env = python2.withPackages (ps: with ps; [
-      pandocfilters
-      pygraphviz
-    ]);
-  };
-}
