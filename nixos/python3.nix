f27acf34869480890e234b72a87f1869e64cd164
Amy de Buitléir <amy@nualeargais.ie>
2021-11-15 20:46:09 +0000

    streamline

--- nixos/python3.nix
+++ nixos/python3.nix
@@ -1,38 +1,15 @@
 {
   nixpkgs.config.packageOverrides = pkgs: with pkgs; {
     python3Env = python3.withPackages (ps: with ps; [
-      # black
       csvkit
-#      gensim
-#      h5py
-#      hmmlearn
-      hiyapyco
-#      ipython
-#      JayDeBeApi
-      jupyterlab
-#      matplotlib
-#      networkx
+      # jupyterlab
       numpy
       pandas
       pandocfilters
-      pdfx
-      pip
-#      pomegranate
-#      pycairo
-#      pydot
-#      pydotplus
       pygraphviz
       pylint
-      # pymc3 - broken in 20.03
-#      pygments
-#      pyqt5
-#      pyro-ppl
-#      pytorch
       scikitlearn
       seaborn
-      setuptools
-      virtualenvwrapper # for pip
-      yamllint
     ]);
   };
 }
