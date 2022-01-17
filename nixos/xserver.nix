5ed40f72f8be8e71080d81dfc188bb8b7f87a79c
Amy de Buitléir <amy@nualeargais.ie>
2022-01-17 16:38:05 +0000

    removed unused window managers

--- nixos/xserver.nix
+++ nixos/xserver.nix
@@ -5,19 +5,19 @@
     enable = true;
     layout = "ie";
 
-    # XMonad
-    windowManager.xmonad = {
-      enable = true;
-      enableContribAndExtras = true;
-      extraPackages = haskellPackages: [
-        # haskellPackages.xmonad_0_17_0
-        # haskellPackages.xmonad-contrib_0_17_0
-        # haskellPackages.xmonad-extras_0_17_0
-        haskellPackages.xmonad
-        haskellPackages.xmonad-contrib
-        haskellPackages.xmonad-extras
-      ];
-    };
+    # # XMonad
+    # windowManager.xmonad = {
+    #   enable = true;
+    #   enableContribAndExtras = true;
+    #   extraPackages = haskellPackages: [
+    #     # haskellPackages.xmonad_0_17_0
+    #     # haskellPackages.xmonad-contrib_0_17_0
+    #     # haskellPackages.xmonad-extras_0_17_0
+    #     haskellPackages.xmonad
+    #     haskellPackages.xmonad-contrib
+    #     haskellPackages.xmonad-extras
+    #   ];
+    # };
 
     windowManager.qtile = {
       enable = true;
@@ -27,10 +27,10 @@
     displayManager.sddm.enable = true;
     desktopManager.plasma5.enable = true;
 
-    displayManager.sessionCommands =
-      "
-          xsetroot -solid '#FFE0FF'
-      ";
+    # displayManager.sessionCommands =
+    #   "
+    #       xsetroot -solid '#FFE0FF'
+    #   ";
   };
 
   
