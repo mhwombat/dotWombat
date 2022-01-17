def9c9d3b53eca25ab61d22a9c159875d0530a92
Amy de Buitléir <amy@nualeargais.ie>
2022-01-17 16:35:50 +0000

    initial commit

---
+++ .config/qtile/colour.py
@@ -0,0 +1,18 @@
+class Colour:
+
+    base00 = '#080a0b'
+    base01 = '#1e2329'
+    base02 = '#38424c'
+    base03 = '#495663'
+    base04 = '#a1adb9'
+    base05 = '#f3f4f6'
+    base06 = '#f9f9fa'
+    base07 = '#fcfcfc'
+    base08 = '#ffcc99'
+    base09 = '#99ffe5'
+    base0A = '#99ccff'
+    base0B = '#ff99fe'
+    base0C = '#e5ff99'
+    base0D = '#b299ff'
+    base0E = '#ff99b2'
+    base0F = '#99ff99'
