eea992d75f803c6765fb694ff0804d030f297ae7
Amy de Buitléir <amy@nualeargais.ie>
2022-02-19 21:38:47 +0000

    custom pandoc filter

--- nix-overlays/default.nix
+++ nix-overlays/default.nix
@@ -1,4 +1,5 @@
 self: super: {
   hello-amy = self.callPackage ./hello-amy {};
   jot = self.haskellPackages.callPackage ./jot {};
+  pandoc-select-code = self.haskellPackages.callPackage ./pandoc-select-code {};
 }
