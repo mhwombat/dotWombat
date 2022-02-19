60a03e537567221300e8ba848d9652bbda97a65f
Amy de Buitléir <amy@nualeargais.ie>
2022-02-19 21:35:49 +0000

    emacs revamp

--- nixos/emacs.nix
+++ nixos/emacs.nix
@@ -1,16 +1,6 @@
 /*
-This is a nix expression to build Emacs and some Emacs packages I like
-from source on any distribution where Nix is installed. This will install
-all the dependencies from the nixpkgs repository and build the binary files
-without interfering with the host distribution.
-
-To build the project, type the following from the current directory:
-
-  nix-build emacs.nix
-
-To run the newly compiled executable:
-
-  ./result/bin/emacs
+See the example at
+https://nixos.org/manual/nixos/stable/index.html#module-services-emacs
 
 To find out what packages are available for Emacs:
 
@@ -26,36 +16,19 @@ let
   myEmacs = pkgs.emacs;
   emacsWithPackages = (pkgs.emacsPackagesNgGen myEmacs).emacsWithPackages;
 in
-  emacsWithPackages (epkgs: (with epkgs.melpaStablePackages; [
-    auto-complete
-    fill-column-indicator    # show fill column
-    haskell-mode             # Haskell editing mode
-    hydra                    # key binding families
-    lv                       # hints for hydra
-    # popup                    # popup tooltips and menus
-  ]) ++ (with epkgs.melpaPackages; [
-    # ample-theme              # Dark-ish theme
-    # async                    # needed by helm
-    # atom-one-dark-theme      # Dark-ish theme
-    base16-theme             # base16 theme schema
-    blacken                  # Python editing mode
-    cyberpunk-theme          # Dark-ish theme
-    # ess
-    fuzzy-finder             # Interface to fzy
-    # helm                     # incremental completion
-    # helm-idris               # Idris doc search using Helm
-    # idris-mode               # Idris editing mode
-    jazz-theme               # Dark-ish theme
-    # material-theme           # Dark-ish theme
-    monokai-theme            # Dark-ish theme
-    markdown-mode            # Markdown editing mode
-    nix-mode                 # Nix editing mode
-    # pandoc-mode              # Pandoc editing mode
-    # proof-general            # for use with Coq
-    # zenburn-theme            # Dark-ish theme
-  ]) ++ (with epkgs.elpaPackages; [
-    # auctex                   # LaTeX mode
-    undo-tree                # Undo/redo that behaves like other apps
-  ]) ++ [
-    # nothing from main packages set
+  emacsWithPackages (epkgs: [
+    pkgs.emacs27Packages.base16-theme             # base16 theme schema
+    pkgs.emacs27Packages.company                  # COMplete ANYthing
+    pkgs.emacs27Packages.expand-region            # expand selections
+    pkgs.emacs27Packages.fill-column-indicator    # show fill column
+    pkgs.emacs27Packages.hydra                    # key binding families
+    pkgs.emacs27Packages.lsp-haskell
+    pkgs.emacs27Packages.lsp-mode
+    pkgs.emacs27Packages.lv                       # hints for hydra
+    pkgs.emacs27Packages.markdown-mode            # Markdown editing mode
+    pkgs.emacs27Packages.undo-tree                # Undo/redo that behaves like other apps
+    pkgs.emacs27Packages.yasnippet                # Used by company
+    # pkgs.emacs27Packages.fuzzy-finder             # Interface to fzy
+    # pkgs.emacs27Packages.haskell-mode             # Haskell editing mode
+    # pkgs.emacs27Packages.nix-mode                 # Nix editing mode
   ])
