5e32230fcc8ebe63cd345cba68c70c65baaba385
Amy de Buitléir <amy@nualeargais.ie>
2023-07-31 14:34:59 +0100

    drop emacs

--- .config/emacs/amy/python.el
+++
@@ -1,9 +0,0 @@
-;;
-;; python
-;;
-
-;; Fill column indicator
-(add-hook 'python-mode-hook 'fci-mode)
-
-;; Remove trailing whitespace
-(add-hook 'python-mode-hook 'add-remove-trailing-whitespace-hook)
